fadfab1592304b134e90ebe73f27f692ca737096
BoomKitty08 <julietBehrens08@Gmail.com>
2026-08-25 17:02:31 -0400

    I wonder if this will work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BiobuzzTest/IntakeTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.BiobuzzTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Intake Test")
+public class IntakeTest extends LinearOpMode {
+
+    private DcMotor intakeMotor;
+
+    @Override
+    public void runOpMode() {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad2.right_trigger > 0.1) {
+                intakeMotor.setPower(1.0);
+            }
+                intakeMotor.setPower(0);
+            }
+        }
+    }
+
+
+
+
+
+
+
