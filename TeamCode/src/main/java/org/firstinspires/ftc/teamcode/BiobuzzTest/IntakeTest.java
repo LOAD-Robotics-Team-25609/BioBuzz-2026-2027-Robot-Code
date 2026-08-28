b4ba70dbe797d6a439522fd0ab1816a31078633b
BoomKitty08 <julietBehrens08@Gmail.com>
2026-08-28 16:55:13 -0400

    fun

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BiobuzzTest/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BiobuzzTest/IntakeTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.BiobuzzTest;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Intake Test")
 public class IntakeTest extends LinearOpMode {
@@ -12,7 +13,7 @@ public class IntakeTest extends LinearOpMode {
     @Override
     public void runOpMode() {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
-        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotor.setDirection(DcMotor.Direction.REVERSE);
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
